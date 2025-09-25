aa44ebf022df4bc5d55c26f0bf6baa5c5fd1d1be
Sebastian Zapart <sebastian.zapart@gmail.com>
2025-09-25 14:55:59 +0200

    Working on graphics pipeline

--- headers/Render/Vk/VkGraphicsPipeline.h
+++ headers/Render/Vk/VkGraphicsPipeline.h
@@ -5,13 +5,32 @@
 // Setts up the graphics pipeline for vk rendering.
 void SetupGraphicsPipeline(VkDevice _device);
 
-const char* ReadShaderFile(const char* _filePath, const char* _type);
+uint32_t* ReadShaderFile(const char* _filePath, const char* _type, size_t* _codeSize);
 
 long GetFileSize(const char* _filePath, const char* _type);
 
-VkShaderModule SetupShaderModule(VkDevice _device, const char* _code);
+// Creats the shader modules by importing the binary spv(SPIR-V) shader file data. Shader modules are just a thin wrapper around the bytecode
+// they can be local verriables since they linking and compilation makes the local veriables unnessasry. 
+// @param _device is the vk device.
+// @param _code is the bytecode of the SPIR-V as a uint32 array.
+// @param _codeSize is the size of the array in bytes.
+// @return Is the vk shader module. 
+VkShaderModule SetupShaderModule(VkDevice _device, uint32_t* _code, size_t _codeSize);
 
-void PopulateShaderModule(VkShaderModuleCreateInfo* _createInfo, const char* _code);
+// Populates the shader modules create info with the spv code and info. 
+void PopulateShaderModule(VkShaderModuleCreateInfo* _createInfo, uint32_t* _code, size_t _codeSize);
+
+// Assign the vertex shader modules to the pipeline using the pipeline shader stage create infor struct. 
+VkPipelineShaderStageCreateInfo GetPipelineVertexShaderStageCreateInfo(VkShaderModule _vertexShaderModule);
+
+// Assign the fragment shader modules to the pipeline using the pipeline shader stage create infor struct. 
+VkPipelineShaderStageCreateInfo GetPipelineFragmentShaderStageCreateInfo(VkShaderModule _fragmentShaderModule);
+
+// Since most of the pipeline is baked into the pipline state, we need to manualy define states that can be changed in run time.
+VkPipelineDynamicStateCreateInfo GetPiplineDynamicStateCreateInfo();
+
+// Gets the vertex imput state create info for the vk graphics pipeline.
+VkPipelineVertexInputStateCreateInfo GetPipelineVertexInputStateCreateInfo();
 
 // Cleans up the Vk Graphics pipeline
 void CleanupGraphicsPipeline();
--- src/Render/Vk/VkGraphicsPipeline.c
+++ src/Render/Vk/VkGraphicsPipeline.c
@@ -8,29 +8,61 @@ void SetupGraphicsPipeline(VkDevice _device){
    
     LOG_INFO("Setup VkGraphicsPipeline");
     
-    const char* vertexSource = ReadShaderFile("shaders/vert.spv", "rb");
-    const char* fragSource = ReadShaderFile("shaders/frag.spv", "rb");
+    size_t vertexCodeSize = 0;
+    size_t fragmentCodeSize = 0;
 
-    VkShaderModule vertexShaderModule = SetupShaderModule(_device, vertexSource);
-    VkShaderModule fragmentShaderModule = SetupShaderModule(_device, fragSource);
+    uint32_t* vertexSource = ReadShaderFile("shaders/vert.spv", "rb", &vertexCodeSize);
+    uint32_t* fragSource = ReadShaderFile("shaders/frag.spv", "rb", &fragmentCodeSize);
 
+    VkShaderModule vertexShaderModule = SetupShaderModule(_device, vertexSource, vertexCodeSize);
+    VkShaderModule fragmentShaderModule = SetupShaderModule(_device, fragSource, fragmentCodeSize);
+
+    VkPipelineShaderStageCreateInfo shaderStagesCreateInfo[2] = {
+        PopulatePipelineVertexShaderStageCreateInfo(vertexShaderModule),
+        PopulatePipelineFragmentShaderStageCreateInfo(fragmentShaderModule)
+    };
+
+    vkDestroyShaderModule(_device, vertexShaderModule, NULL);
+    vkDestroyShaderModule(_device, fragmentShaderModule, NULL);
+    free(vertexSource);
+    vertexSource = NULL;
+    free(fragSource);
+    fragSource = NULL;
     return;
 };
 
-const char* ReadShaderFile(const char* _filePath, const char* _type){
+uint32_t* ReadShaderFile(const char* _filePath, const char* _type, size_t* _codeSize){
 
     long filesize = GetFileSize(_filePath, _type);
-    char* buffer = malloc(sizeof(const char) * filesize);
+    uint32_t* buffer = (uint32_t*)malloc(filesize);
+
+    if(filesize <= 0){
+        LOG_ERROR("File size 0 or smaller, failed alloc");
+        return NULL;
+    }
+    if(!buffer){
+        LOG_ERROR("Buffer memory allocation faild");
+        return NULL;
+    }
 
     FILE* fptr = fopen(_filePath, _type);
+    size_t bytesRed = 0;
     if(fptr){
 
-        fgets(buffer, filesize, fptr); // read into buffer, amount of size and from the file ptr.
+        bytesRed = fread(buffer, 1, filesize, fptr); // read into buffer, amount of size and from the file ptr.
         fclose(fptr);
     } else {
         LOG_ERROR("Faild to open file: %s", _filePath);
         return 0;
     }   
+
+    if(bytesRed != filesize){
+        LOG_ERROR("Faild to read complete shader file: %s", _filePath);
+    }
+
+    if(_codeSize){
+        *_codeSize = filesize;
+    }
 
     return buffer; 
 };
@@ -43,7 +75,6 @@ long GetFileSize(const char* _filePath, const char* _type){
 
         fseek(fptr, 0L, SEEK_END); // set file ptr to end of file
         fileSize = ftell(fptr); // retrives the position of file ptr, representing size
-    
         fclose(fptr);
     } else {
         LOG_ERROR("Faild to open file: %s", _filePath);
@@ -53,14 +84,13 @@ long GetFileSize(const char* _filePath, const char* _type){
     return fileSize;
 };
 
-VkShaderModule SetupShaderModule(VkDevice _device, const char* _code){
+VkShaderModule SetupShaderModule(VkDevice _device, uint32_t* _code, size_t _codeSize){
 
     VkShaderModule shaderModule = {0};
 
     VkShaderModuleCreateInfo createInfo = {0};
-    
 
-    PopulateShaderModule(&createInfo, _code);
+    PopulateShaderModule(&createInfo, _code, _codeSize);
     if (vkCreateShaderModule(_device, &createInfo, NULL, &shaderModule) != VK_SUCCESS) {
         LOG_ERROR("Faild to create shader module");
         return VK_NULL_HANDLE;
@@ -69,13 +99,59 @@ VkShaderModule SetupShaderModule(VkDevice _device, const char* _code){
     return shaderModule;
 };
 
-void PopulateShaderModule(VkShaderModuleCreateInfo* _createInfo, const char* _code){
-
-    size_t codeSize = strlen(_code);
+void PopulateShaderModule(VkShaderModuleCreateInfo* _createInfo, uint32_t* _code, size_t _codeSize){
 
     _createInfo->sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
-    _createInfo->codeSize = codeSize;
-    _createInfo->pCode = (const uint32_t*)_code;
+    _createInfo->codeSize = _codeSize;
+    _createInfo->pCode = _code;
+};
+
+VkPipelineShaderStageCreateInfo PopulatePipelineVertexShaderStageCreateInfo(VkShaderModule _vertexShaderModule){
+    VkPipelineShaderStageCreateInfo vertShaderStageInfo = {0};
+    // first step is telling vk what pipline stage the shader is going to be used in. 
+    vertShaderStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+    vertShaderStageInfo.stage = VK_SHADER_STAGE_VERTEX_BIT;
+    // next is the module containg the code, and the entry point of the code, being "main", set as the name.
+    vertShaderStageInfo.module = _vertexShaderModule;
+    vertShaderStageInfo.pName = "main";
+    // the specializationInfo can specify values for shader constatnts before runtime. 
+    //Meaning it can optimize better, left at null for now
+    vertShaderStageInfo.pSpecializationInfo = NULL; 
+    return vertShaderStageInfo;
+};
+
+VkPipelineShaderStageCreateInfo PopulatePipelineFragmentShaderStageCreateInfo(VkShaderModule _fragmentShaderModule){
+
+    VkPipelineShaderStageCreateInfo fragShaderStageInfo = {0};
+    // same info for fragment shader as vertex shader
+    fragShaderStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+    fragShaderStageInfo.stage = VK_SHADER_STAGE_FRAGMENT_BIT;
+    fragShaderStageInfo.module = _fragmentShaderModule;
+    fragShaderStageInfo.pName = "main";
+    fragShaderStageInfo.pSpecializationInfo = NULL; 
+    return fragShaderStageInfo;
+};
+
+VkPipelineDynamicStateCreateInfo GetPiplineDynamicStateCreateInfo(){
+        
+    // These are the states we wish to define as dynamic
+    VkDynamicState dynamicStates[2] = {
+        VK_DYNAMIC_STATE_VIEWPORT,
+        VK_DYNAMIC_STATE_SCISSOR
+    };
+
+    // This will cause these values to be ignored when initalizing, 
+    // so that they can be configured during runtime. 
+    VkPipelineDynamicStateCreateInfo dynamicState = {0};
+    dynamicState.sType = VK_STRUCTURE_TYPE_PIPELINE_DYNAMIC_STATE_CREATE_INFO;
+    dynamicState.dynamicStateCount = 2;
+    dynamicState.pDynamicStates = dynamicStates;
+
+    return dynamicState;
+};
+
+VkPipelineVertexInputStateCreateInfo GetPipelineVertexInputStateCreateInfo(){
+
 };
 
 void CleanupGraphicsPipeline(){
